95306538a2eb5ba19a3c776716aeacaeb3eb07f6
Rich Lane <therealrichlane@gmail.com>
2014-11-04 21:22:43 -0700

    how old are you

---
+++ age.rb
@@ -0,0 +1,11 @@
+puts "How old are you these days?"
+age = gets.chomp.to_i
+
+puts "In 5 years you will be:"
+puts age + 5
+
+puts "In 10 years you will be:"
+puts age + 10
+
+puts "In 15 years you will be:"
+puts age + 15
